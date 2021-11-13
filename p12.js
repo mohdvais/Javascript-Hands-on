7c30340679720e97c4798c0484a30b1d9ccd02b8
MOHD_VAIS <mohammadvais2@gmail.com>
2021-11-13 15:57:47 +0530

    practice

--- p12.js
+++ p12.js
@@ -1,19 +1,35 @@
 console.log('Find the largest among three numbers');
-const num1 = parseFloat(prompt('Enter first number: '));
-const num2 = parseFloat(prompt('Enter second number: '));
-const num3 = parseFloat(prompt('Enter third number: '));
+// const num1 = parseFloat(prompt('Enter first number: '));
+// const num2 = parseFloat(prompt('Enter second number: '));
+// const num3 = parseFloat(prompt('Enter third number: '));
 
-let largest;
-//check the condition
+// let largest;
+// //check the condition
 
-if (num1>= num2 && num1>=num3){
-    largest =num1;
-}
-else if (num2 >= num1 && num1>= num3){
-    largest = num2;
-}
-else{
-    largest =num3;
-}
-//display the result
-console.log(`The largest value is ${largest}`);
+// if (num1>= num2 && num1>=num3){
+//     largest =num1;
+// }
+// else if (num2 >= num1 && num1>= num3){
+//     largest = num2;
+// }
+// else{
+//     largest =num3;
+// }
+// //display the result
+// console.log(`The largest value is ${largest}`);
+
+
+
+// // using Math.max() 
+// const num1 = parseFloat(prompt('Enter first number: '));
+// const num2 = parseFloat(prompt('Enter second number: '));
+// const num3 = parseFloat(prompt('Enter third number: '));
+
+// const largest = Math.max(num1, num2, num3);
+
+// console.log("The largest number is " + largest);
+
+
+
+
+
--- tut10.js
+++ tut10.js
@@ -1,16 +1,42 @@
 console.log("Function in Javascript");
 
 function greet(name, thank){
-    console.log(
+    
+        let msg = 
         `Happy Birthday ${name} How i wish I could fly to you right now
          and be with you on this special day of yours. but remember,
-         my good wishes are always there with you.${thank} Happy birthday!`
-      );
+         my good wishes are always there with you. ${thank}`;
+         return msg;
+      
 
 }
 let name = "skillF";
 let name2= "Rohan";
 
-greet(name,'Thanks a lot');
+let val = greet(name,'Thanks a lot');
+console.log(val);
 
 
+const myobj ={
+    name: 'skillf',
+    game: function(){
+        return 'GTA';
+    }
+}
+console.log(myobj.game())
+
+arr = ['fruit','vegetable','furniture'];
+arr.forEach(function(element,index,array){
+    console.log(element,array,index)
+});
+
+
+var i =234;
+console.log(i);
+function ui(name)
+{
+    var i=9;
+    console.log(i)
+    return `This is a ${name} ui`;
+}
+console.log(ui("harry"),i)
